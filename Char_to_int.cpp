f47eed55a1b6518b093f0d88e8169c32858ede72
Diana Ivanchevska <ivanchevska.diana@gmail.com>
2020-06-24 11:56:42 +0300

    + Pointer

--- Char_to_int.cpp
+++ Char_to_int.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
-int char_to_int(const char symbol)
+int char_to_int(const char *symbol)
 {
-    return symbol;
+    return *symbol;
 }
 
 int main()
 
 {
-    char symbol = '^';
-    cout<< char_to_int(symbol)<<endl;
+    char symbol = '+';
+    char *psymbol = &symbol;
+    cout<< char_to_int(psymbol)<<endl;
     /*char symbol = '%';
     cout << (int)symbol << endl;*/
     /*for (int i = 0; i <= 255; i++) 
